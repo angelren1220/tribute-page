fb8d1875ab1fbe990cbf37d76aac4b0dc7ec7fa9
Linuxe <angelren1220@gmail.com>
2023-08-28 04:47:04 -0400

    add album

--- front-end/src/components/Album.js
+++ front-end/src/components/Album.js
@@ -1,7 +1,22 @@
-function Album(){
-  return(
-    <>this is album page</>
-  )
+import '../styles/Album.scss';
+
+function Album() {
+  return (
+    <div className="album-container">
+      <div className="youtube-video">
+        <iframe width="800" height="450" src="https://www.youtube.com/embed/322wQTwVYwM?si=cPI-7C4ji0Sz6QKX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
+      <div className="youtube-video">
+        <iframe width="800" height="450" src="https://www.youtube.com/embed/CpURgXug9vk?si=0FMUesdsMbQzbRSE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
+      <div className="youtube-video">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/MZdxy7_lc8U?si=kk-tXIPKh1I0eNzU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
+      <div className="youtube-video">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/fD0IIMRHpAQ?si=SzLyo0jfH5yJeKyJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
+    </div>
+  );
 }
 
 export default Album;
